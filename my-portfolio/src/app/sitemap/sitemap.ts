b2afa8a127a96772ab6ad4467d406893f9f06277
Nwaforchuks <129457774+Nwaforchuks@users.noreply.github.com>
2026-07-21 14:27:05 +0100

    Add sitemap generation for SEO

---
+++ my-portfolio/src/app/sitemap/sitemap.ts
@@ -0,0 +1,16 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://constantineapp.vercel.app'
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    
+  ]
+}
